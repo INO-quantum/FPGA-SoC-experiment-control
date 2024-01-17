b128765da15e072c6c6d84469b45090f3b8d8a22
AndiT0815 <andit0815@gmail.com>
2024-01-17 11:04:40 +0100

    update labscript-suite

--- labscript-suite/userlib/labscriptlib/FPGA_test/FPGA_test.py
+++ labscript-suite/userlib/labscriptlib/FPGA_test/FPGA_test.py
@@ -16,13 +16,14 @@ from labscriptlib.FPGA_test.connection_table import primary, secondary
 ########################################################################################################################
 # experimental sequence
 
-# load do and ao from globals.
-# in a typical experiment you give a proper name of you channel in connection_table.
-g = globals()['__builtins__'] # somehow labscript stores these as builtins?
-num_analog = 4 if secondary is None else 8
-num_digial = 16 if secondary is None else 32
-do = [g['do%i'%i] for i in range(num_digital)] 
-ao = [g['ao%i'%i] for i in range(num_analog)]
+if True:
+    # load do and ao channesl from globals.
+    # in a typical experiment you give a proper name of your channel in connection_table. so this is not needed.
+    g = globals()['__builtins__'] # somehow labscript stores these as builtins?
+    num_analog = 4 if secondary is None else 8
+    num_digital = 16 if secondary is None else 32
+    do = [g['do%i'%i] for i in range(num_digital)]
+    ao = [g['ao%i'%i] for i in range(num_analog)]
 
 if __name__ == '__main__':
 
@@ -32,11 +33,40 @@ if __name__ == '__main__':
     # start sequence
     start()
 
+    if False:
+        # these are some examples of options which can be set for each experiment.
+        # this is convenient for debugging but permanent settings should be done in connection table worker_args.
+
+        # set start, data stop and restart trigger
+        # this is an example for a re-triggerable line-trigger at the start and during experiment
+        # you can also use another input as start trigger and set in first data bit 30 (see WAIT command below).
+        # this way you can wait for MOT loaded and start with next line trigger.
+        primary.set_start_trigger('input 2', 'rising edge')          # start experiment with rising edge at input 2.
+        primary.set_stop_trigger('data bits 28-31','offset bit 2')   # stop experiment when data bit 30 is set.
+        primary.set_restart_trigger('input 2', 'rising edge')        # restart experiment with next rising edge on input 2.
+
+        # this sets stop bit 30 in data at the given time.
+        # digital or analog channels can be still programmed at the same time.
+        primary.WAIT(t)
+
+        # for debugging its nice to have a signal when board is running or waiting
+        # 'run' = high while running and restart trigger, low while waiting for start trigger
+        # 'wait' = high while waiting for start or restart trigger
+        # note: depending on board version output buffer is inverting
+        primary.set_ctrl_out({'output 1': ('wait', 'low level')})
+
+        # set synchronization primary wait time and secondary phase
+        # for primary board use wait time >0 in units of 10ns and phase = 0
+        # for secondary board use wait time = 0 and phase_ext can be used to fine-tune on the ns level.
+        # phase_det is normally 0 but if you see random jumps of +/-10ns you can adjust this.
+        # units of phase = 0..560 in steps of 1 = 0..360° in steps of 360/560°. 360° = 10ns.
+        primary.set_sync_params(wait = 0, phase_ext = 0, phase_det = 0)
+
     # define some experiment parameters
     do_ramp              = True     # perform triangular ramp on all analog channels
     ramp_rate            = 100e3    # sample rate in Hz
     wait_between_digital = False    # wait for each channel when switching digital channels on/off
-    experiment_time      = 3.0      # (approximate) experiment time in seconds
+    experiment_time      = 3.0      # experiment time in seconds
 
     # switch all digital outputs on
     # this can be done all together or one by one
@@ -47,15 +77,13 @@ if __name__ == '__main__':
                 t += dt
                 
     if do_ramp:
-        # do a triangular ramp on all channels separated by dt
-        t_start = t
-        duration = experiment_time - t - dt
-        for i,aoi in enumerate(ao):
-            t = t_start + i*dt
-            t += aoi.ramp(t=t, initial=  0, final= 10, duration=duration/4, samplerate=ramp_rate) + 1/ramp_rate
-            t += aoi.ramp(t=t, initial= 10, final=-10, duration=duration/2, samplerate=ramp_rate) + 1/ramp_rate
-            t += aoi.ramp(t=t, initial=-10, final=  0, duration=duration/4, samplerate=ramp_rate) + dt
-    
+        # do a triangular ramp on one analog channel
+        channel = ao0
+        t += dt
+        duration = experiment_time - t - 2*dt - 2/ramp_rate
+        t += channel.ramp(t=t, initial=  0, final= 10, duration=duration/4, samplerate=ramp_rate) + 1/ramp_rate
+        t += channel.ramp(t=t, initial= 10, final=-10, duration=duration/2, samplerate=ramp_rate) + 1/ramp_rate
+        t += channel.ramp(t=t, initial=-10, final=  0, duration=duration/4, samplerate=ramp_rate) + dt
     else:
 
         # switch all analog outputs to constant values
@@ -85,7 +113,7 @@ if __name__ == '__main__':
     # switch all digital outputs off
     # this can be done all together or one by one
     if True:
-        t = experiment_time
+        t = experiment_time - ((len(do)+1)*dt if wait_between_digital else dt)
         for doi in do:
             doi.go_low(t)
             if wait_between_digital:
--- labscript-suite/userlib/labscriptlib/FPGA_test/connection_table.py
+++ labscript-suite/userlib/labscriptlib/FPGA_test/connection_table.py
@@ -11,63 +11,82 @@ from user_devices.generic_conversion import generic_conversion
 ########################################################################################################################
 # FPGA boards
 
+# examples for worker_args:
+# these can can be overwritten in experiment script and in GUI.
+# 'inputs' : {'start trigger': ('input 1', 'rising edge')} # needed for secondary board
+# 'outputs': {'output 0': ('sync out', 'low level')}, # needed for primary board
+# 'ext_clock' : True,  # True = use external clock
+# 'ignore_clock_loss' : True # True = ignore when external clock is lost. Message Box is displayed but board keeps running.
+
 # primary board:
 # note: importing 'primary' from connection_table does not work with 'FPGA_device(name='primary',...)' but requres to assign primary!
-primary = FPGA_board(name='primary', ip_address="192.168.1.120", ip_port=DEFAULT_PORT, bus_rate=1e6, num_racks=1,trigger_device=pb0_trg,
-    worker_args={ # optional arguments. can be changed in GUI. if defined here used at startup, otherwise last settings in GUI are used.
-        #'inputs':{'start trigger': ('input 1', 'rising edge')},
-        #'outputs': {'output 0': ('sync out', 'low level')}, # required: start trigger for sec. board (default, keep here). other outputs can be added.
-        #'ext_clock':True,  # True = use external clock
-        #'ignore_clock_loss':True # True = ignore when external clock is lost. Message Box is displayed but board keeps running.
-        #'trigger':{}, # no trigger (default)
-        #'trigger':{'start trigger':('input 0', 'rising edge')}, # start trigger
-        #'trigger':{'start trigger':('input 0', 'rising edge'),'stop trigger':('input 1', 'falling edge'),'restart trigger':('input 0', 'rising edge')}, # start+stop+restart trigger
-    }
+primary = FPGA_board(
+    name           = 'primary',         # give board name displayed in GUI. must be valid Python variable name.
+    ip_address     = "192.168.1.140",   # IP address of board
+    ip_port        =   DEFAULT_PORT,    # port of the board. can be skipped, None, DEFAULT_PORT
+    bus_rate       = 1e6,               # bus output rate in Hz. typically 1MHz
+    num_racks      = 1,                 # number of sub-racks the board is connected to. 1 or 2.
+    trigger_device = None,              # None for the primary board, primary board for secondary board.
+    worker_args    = {                  # optional settings
+        'outputs': {'output 0': ('sync out', 'low level')}, # trigger secondary board on experiment start.
+        'ext_clock': False,             # if True use external clock reference
+        }
     )
 
 # secondary board: (enable with True)
 if False:
-    secondary = FPGA_board(name='secondary', ip_address="192.168.1.131", ip_port=DEFAULT_PORT, bus_rate=1e6, num_racks=1, trigger_device=primary,
-        worker_args={ # optional arguments. can be changed in GUI. if defined here used at startup, otherwise last settings in GUI are used.
-            #'inputs':{'start trigger':('input 0', 'falling edge')},
-                      #'NOP bit':('data bits 20-23','offset bit 0'),
-                      #'STRB bit' : ('data bits 20-23','offset bit 3')}, # required: start trigger from primary board (default, keep here)
-            #'ext_clock': True,  # True = required: use external clock from primary board (default, keep here)
-            #'ignore_clock_loss': True # True = ignore when external clock is lost. Message Box is displayed but board keeps running.
-            # 'trigger':{'start trigger':('input 0', 'falling edge'),'stop trigger':('input 1', 'falling edge'),'restart trigger':('input 1', 'rising edge')}, # start+stop+restart trigger. must be input 1 or 2
-            #'outputs':{'output 0': ('sync out', 'low level'),'output 0': ('lock lost', 'high level')}, example outputs can be added when needed.
+    secondary = FPGA_board(
+        name            = 'secondary',      # give board name displayed in GUI. must be valid Python variable name.
+        ip_address      = "192.168.1.141",  # IP address of board
+        ip_port         = DEFAULT_PORT,     # port of the board. can be skipped, None, DEFAULT_PORT
+        bus_rate        = 1e6,              # bus output rate in Hz. typically 1MHz
+        num_racks       = 1,                # number of sub-racks the board is connected to. 1 or 2.
+        trigger_device  = primary,          # must be primary board name
+        worker_args     = {                 # optional settings
+            'inputs' : {'start trigger' : ('input 0', 'falling edge')}, # use start trigger from primary board on input 0
+            'ext_clock': True,              # use external clock from primary board.
         })
+else: secondary = None
 
 ########################################################################################################################
 # analog outputs
+# in a real experiment you would give a name like 'MOT_current' to each channel.
+# connection = address set on sub-rack. can be decimal or hex - as you prefer.
 
 AnalogChannels(name='AO0', parent_device=primary, rack=0, max_channels=2)
-AnalogOut     (name='ao0', parent_device=AO0, connection='0x00')
-AnalogOut     (name='ao1', parent_device=AO0, connection='0x01')
+AnalogOut     (name='ao0', parent_device=AO0, connection='0x03')
+AnalogOut     (name='ao1', parent_device=AO0, connection='0x04')
 
+# example with generic_conversion class.
+# use variable 'x' to convert from 'unit' to volts in 'equation'.
+# 'min' and 'max' give the limits in the chosen 'unit'.
 AnalogChannels(name='AO1', parent_device=primary, rack=0, max_channels=2)
-AnalogOut     (name='ao2', parent_device=AO1, connection='0x02', unit_conversion_class=generic_conversion,
-               unit_conversion_parameters={'unit':'A', 'equation':'x', 'min':-10.0, 'max':10.0})
-AnalogOut     (name='ao3', parent_device=AO1, connection='0x03', unit_conversion_class=generic_conversion,
+AnalogOut     (name='ao2', parent_device=AO1, connection='0x05', unit_conversion_class=generic_conversion,
+               unit_conversion_parameters={'unit':'A', 'equation':'x/10.0', 'min':-0.01, 'max':100.0})
+AnalogOut     (name='ao3', parent_device=AO1, connection='0x06', unit_conversion_class=generic_conversion,
                unit_conversion_parameters={'unit':'MHz', 'equation':'(x-142.31036)/31.31786', 'min':0.0, 'max':455.48896})
 
 if secondary is not None:
-    AnalogChannels(name='AO2', parent_device=primary, rack=0, max_channels=4)
-    AnalogOut     (name='ao4', parent_device=AO0, connection='0x04')
-    AnalogOut     (name='ao5', parent_device=AO0, connection='0x05')
-    AnalogOut     (name='ao6', parent_device=AO0, connection='0x06')
-    AnalogOut     (name='ao7', parent_device=AO0, connection='0x07')
+    AnalogChannels(name='AO2', parent_device=secondary, rack=0, max_channels=4)
+    AnalogOut     (name='ao4', parent_device=AO0, connection='0x07')
+    AnalogOut     (name='ao5', parent_device=AO0, connection='0x08')
+    AnalogOut     (name='ao6', parent_device=AO0, connection='0x09')
+    AnalogOut     (name='ao7', parent_device=AO0, connection='0x0a')
 
 ########################################################################################################################
 # digital outputs
+# in a real experiment you would give a name like 'MOT_IGBT' to each channel.
+# connection on DigitalChannels = address set on the sub-rack. can be decimal or hex - as you prefer.
+# connection on DigitalOut      = channel number 0..max_channels-1
 
 DigitalChannels(name='DO0', parent_device=primary, connection='0x0', rack=0, max_channels=16)
 for i in range(16):
-    DigitalOut(name='do%i'%i, parent_device=DO0, connection=0)
+    DigitalOut(name='do%i'%i, parent_device=DO0, connection=i)
 
 if secondary is not None:
+    DigitalChannels(name='DO1', parent_device=secondary, connection='0x01', rack=0, max_channels=16)
     for i in range(16):
-        DigitalOut(name='do%i'%i+16, parent_device=DO0, connection='0x%02x'%i)
+        DigitalOut(name='do%i'%i+16, parent_device=DO1, connection=i)
 
 ########################################################################################################################
 # experimental sequence
